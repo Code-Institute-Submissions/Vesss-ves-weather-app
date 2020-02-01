6df340193772d7b76c48e2f219eeeda60224055b
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-02-01 00:00:56 +0000

    Create products model - title, desc, price, return as string.title

--- products/models.py
+++ products/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Product(models.Model):
     title           = models.CharField(max_length=130)
     description     = models.TextField()
-    price           = models.DecimalField(decimal_places=2, max_digits=4, null=39.99)
+    price           = models.DecimalField(decimal_places=2, max_digits=4, default=39.99)
+    
+    def __str__(self):
+        return self.title
     
