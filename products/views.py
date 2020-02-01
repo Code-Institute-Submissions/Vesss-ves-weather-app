a989f03f219654199f7e1d70d29f7d3d0832d92a
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-02-01 13:47:14 +0000

    Create new class-based view for product details.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -7,3 +7,10 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/products.html"
 
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/product-details.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        return context
