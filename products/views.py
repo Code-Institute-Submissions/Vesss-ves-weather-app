ae566569a1e72f3af95c40246ced1dc09a96cfa6
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-02-01 00:01:55 +0000

    Create view for products app - class-based view.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/products.html"
+
