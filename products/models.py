e3a226f03e0caa9c6101753ae7049eb7b42d0ad3
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-02-05 18:15:59 +0000

    Add new model - slug. The slug will be replacing the current id in the url. It's for better SEO optimisation.

--- products/models.py
+++ products/models.py
@@ -41,6 +41,7 @@ class ProductManager(models.Manager):
     
 class Product(models.Model):
     title           = models.CharField(max_length=130)
+    slug            = models.SlugField(blank=True)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=4, default=39.99)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
