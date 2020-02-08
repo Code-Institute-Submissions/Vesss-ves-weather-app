01b1dbb5ef8703877f34407c7af42ad230cde3cb
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-02-08 18:58:02 +0000

    Fiddle with sessions / session keys to get a grip on how they work.

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def cart_home(request):
+    # print(request.session)
+    # print(dir(request.session))
+    key = request.session.session_key
+    print(key)
     return render(request, "cart/cart.html", {})
