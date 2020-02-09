36a35c10b11f9d383ad71f57c476864ad2d2e311
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-02-09 19:34:40 +0000

    Update class CartManager - helps identify logged in and non-logged in users.

--- cart/models.py
+++ cart/models.py
@@ -6,6 +6,14 @@ from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 
+class CartManager(models.Manager):
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated():
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
 class Cart(models.Model):
     user        = models.ForeignKey(User, null=True, blank=True)
     products    = models.ManyToManyField(Product, blank=True)
@@ -13,5 +21,6 @@ class Cart(models.Model):
     timestamp   = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
     
+    objects = CartManager()
     def __str__(self):
         return str(self.id)
