d0d2ee98f2f515cfe3b00f6aa1283b86dc67154b
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-02-15 12:47:22 +0000

    Update pychace and migrations

---
+++ cart/.~c9_invoke_FT3qR5.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+from .models import Cart
+
+def cart_home(request):
+    cart_obj = Cart.objects.new_or_get(request)
+    
+    return render(request, "cart/cart.html", {})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
