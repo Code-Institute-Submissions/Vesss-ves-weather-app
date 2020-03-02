0a7271362208cdddf1d8c69d0a4f28efc28a078a
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-03-02 19:25:37 +0000

    Create model for Orders. Create pre_save_order_id function. Create tuple for order status choices (in effect also part of the model).

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save
 from cart.models import Cart
+from weatherapp.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -22,6 +24,11 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+        
+pre_save.connect(pre_save_create_order_id, sender=Order)
 
 
 # generate the order id
