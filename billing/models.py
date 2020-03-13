8a79f5887080dec6691e8757d8c71bac5ee57c92
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-03-13 12:13:37 +0000

    Improve class BillingProfileManager.

--- billing/models.py
+++ billing/models.py
@@ -4,6 +4,25 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        user = request.user
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+        if user.is_authenticated():
+            'logged in user checkout; remember payment stuff'
+            obj, created = BillingProfile.objects.get_or_create(
+                                    user=user, email=user.email)
+        elif guest_email_id is not None:
+            'guest user checkout; auto reloads payment stuff'
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = BillingProfile.objects.get_or_create(
+                                                email=guest_email_obj.email)
+        else:
+            pass
+        return obj, created
+
 # Create your models here.
 class BillingProfile(models.Model):
     user        = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
@@ -11,6 +30,8 @@ class BillingProfile(models.Model):
     active      = models.BooleanField(default=True)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
+    
+    objects = BillingProfileManager()
     
     def __str__(self):
         return self.email
