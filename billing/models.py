ca139232331dc275919c28e48ea7ec48f0b91e8a
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-03-27 15:36:41 +0000

    Created billing_profile_created_receiver function.

--- billing/models.py
+++ billing/models.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 User = settings.AUTH_USER_MODEL
+
+import stripe
+stripe.api_key = "sk_test_E7QspNeUnEAzM8vfYsPgZGWx003AidX4N6"
 
 class BillingProfileManager(models.Manager):
     def new_or_get(self, request):
@@ -30,16 +33,27 @@ class BillingProfile(models.Model):
     active      = models.BooleanField(default=True)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
+    customer_id = models.CharField(max_length=120, null=True, blank=True)
     
     objects = BillingProfileManager()
     
     def __str__(self):
         return self.email
+        
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+        if not instance.customer_id and instance.email:
+            print("ACTUAL API REQUEST Send to Stripe")
+            customer = stripe.Customer.create(
+            email = instance.email
+                )
+            print(customer)
+            instance.customer_id = customer.id
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
     if created and instance.email:
         BillingProfile.objects.get_or_create(user=instance, email=instance.email)
-        
         
 post_save.connect(user_created_receiver, sender=User)
     
