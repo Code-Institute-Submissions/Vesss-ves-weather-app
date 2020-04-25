60000292bf9528a3c0098b071b59a41a0922f13d
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-04-25 01:39:12 +0000

    stripe configured successfully

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ import stripe
 from urllib.parse import urljoin
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.urls import reverse
 
 from accounts.forms import UserLoginForm, GuestForm
 from accounts.models import GuestEmail
@@ -35,7 +36,7 @@ def cart_update(request):
     return redirect("cart:home") #shortcurt for redirect
 
 def checkout_home(request):
-    stripe_session_id = ''
+    stripe_session_id = request.session.get('stripe_session_id')
     cart_obj, cart_created = Cart.objects.new_or_get(request)
     order_obj = None
     if cart_created or cart_obj.products.count() == 0:
@@ -53,6 +54,7 @@ def checkout_home(request):
         address_qs = Address.objects.filter(billing_profile=billing_profile)
         
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
+        request.session["order_obj_id"] = order_obj.id
         if shipping_address_id:
             order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
             del request.session["shipping_address_id"]
@@ -63,38 +65,39 @@ def checkout_home(request):
             order_obj.save()
     
     # stripe integration
-    try:
-        order_obj_forStripe = Order.objects.get(cart=cart_obj)
-        product_data_forStripe = []
-        host_uri = "{}://{}".format(request.scheme, request.get_host())
-        
-        for item in order_obj_forStripe.cart.products.all():
-            product_data_forStripe.append({
-                'name': item.title,
-                'description': item.description,
-                'images': [urljoin(host_uri, "media/"+str(item.image))],
-                'amount': int(item.price*100),
-                'currency': 'usd',
-                'quantity': 1,
-            })
+    if not stripe_session_id:
+        try:
+            order_obj_forStripe = Order.objects.get(cart=cart_obj)
+            product_data_forStripe = []
+            host_uri = "{}://{}".format(request.scheme, request.get_host())
+            
+            for item in order_obj_forStripe.cart.products.all():
+                product_data_forStripe.append({
+                    'name': item.title,
+                    'description': item.description,
+                    'images': [urljoin(host_uri, "media/"+str(item.image))],
+                    'amount': int(item.price*100),
+                    'currency': 'usd',
+                    'quantity': 1,
+                })
+    
+            # stripe checkout
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            
+            stripeSession = stripe.checkout.Session.create(
+              payment_method_types=['card'],
+              line_items=product_data_forStripe,
+              success_url=urljoin(host_uri, reverse("cart:success") + '?session_id={CHECKOUT_SESSION_ID}'),
+              cancel_url=urljoin(host_uri, 'checkout/cancel'),
+            )
+            stripe_session_id = stripeSession.get('id')
+            request.session['stripe_session_id'] = stripe_session_id
+        except Order.DoesNotExist:
+            # no need stripe if no order
+            pass
 
-        # stripe checkout
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        
-        session = stripe.checkout.Session.create(
-          payment_method_types=['card'],
-          line_items=product_data_forStripe,
-          success_url=urljoin(host_uri, 'checkout/success/?session_id={CHECKOUT_SESSION_ID}'),
-          cancel_url=urljoin(host_uri, 'checkout/cancel'),
-        )
-        # print(session)
-        stripe_session_id = session.get('id')
-        request.session['stripe_session_id'] = stripe_session_id
-    except Order.DoesNotExist:
-        # no need stripe if no order
-        pass
-    
-    
+    """
+    # this section is now moved to checkout done or payment done view
     if request.method == "POST":
         "check that order is done"
         is_done = order_obj.check_done()
@@ -103,6 +106,7 @@ def checkout_home(request):
             request.session['cart_items']=0
             del request.session['cart_id']
             return redirect("cart:success")
+    """
     context = {
         "object": order_obj,
         "billing_profile": billing_profile,
@@ -110,13 +114,30 @@ def checkout_home(request):
         "guest_form": guest_form,
         "address_form": address_form,
         "address_qs": address_qs,
+        "stripe_pub_key": settings.STRIPE_PUBLISHABLE_KEY,
         "stripe_session_id": stripe_session_id,
     }
     return render(request, "cart/checkout.html", context)
     
 
 def checkout_done_view(request):
-    return render(request, "cart/checkout-done.html", {})
+    order_obj_id = request.session.get("order_obj_id")
+    if request.session.get('stripe_session_id') and request.session.get("order_obj_id") and request.session.get("cart_id"):
+        try:
+            order_obj = Order.objects.get(id=order_obj_id)
+            
+            "check that order is done"
+            is_done = order_obj.check_done()
+            if is_done:
+                order_obj.mark_paid()
+                request.session['cart_items']=0
+                del request.session['cart_id']
+                del request.session['stripe_session_id']
+                return render(request, "cart/checkout-done.html", {})
+        except Order.DoesNotExist:
+            pass
+    return redirect("cart:checkout")
+    
     
 
 
