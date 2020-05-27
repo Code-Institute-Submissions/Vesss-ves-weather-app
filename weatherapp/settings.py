f36e74f2421d77e75033d43c1a9609130a3fd275
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2020-05-27 14:56:44 +0000

    Debug = False in settings.py.

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), '0550b9f3bb10472a915c402288dcb1a4.vfs.cloud9.us-east-1.amazonaws.com', 'ves-weather-app.herokuapp.com']
 
