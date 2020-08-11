3b8afbf8575444e4fdfe605ee18b2c393e99728e
Secret <ves.dimitrov121@gmail.com>
2020-08-11 12:52:08 +0600

    donation app code improved

--- donations/views.py
+++ donations/views.py
@@ -1,59 +1,55 @@
-from django.shortcuts import render
-from django.conf import settings
-from django.views.generic.base import TemplateView
-from django.http import JsonResponse
-from django.shortcuts import redirect
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from .models import Donation
-
 import stripe
 import json
 
-    
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Donation
+
+
 class DonationPageView(TemplateView):
+    # Donation page will be a public page
     template_name = 'donations.html'
 
     def get_context_data(self, **kwargs):
         stripe.api_key = settings.STRIPE_SECRET_KEY
         intent = stripe.PaymentIntent.create(
-          amount=1099,
-          currency='usd',
-          # Verify your integration in this guide by including this parameter
-          #metadata={'integration_check': 'accept_a_payment'},
-          setup_future_usage='off_session',
+            amount=1099,
+            currency='usd',
+            setup_future_usage='off_session',
         )
         context = super().get_context_data(**kwargs)
+
+        # pass stripe credentials via context
         context['stripe_pub_key'] = settings.STRIPE_PUBLISHABLE_KEY
         context['client_secret'] = intent.client_secret
         return context
 
 
-class ThankYouPageView(TemplateView):
+@method_decorator(decorator=[csrf_exempt, ], name='dispatch')
+class ThankYouPageView(LoginRequiredMixin, TemplateView):
     template_name = 'thankyou.html'
-    
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
-    
+
     def post(self, *args, **kwargs):
         post_data = json.loads(self.request.POST.get('data'))
-        data = dict((key, post_data[key]) for key in post_data if key!='id')
+        data = dict((key, post_data[key]) for key in post_data if key != 'id')
         data["scid"] = post_data.get("id")
-        print(data)
+        # print(data)
         don = Donation()
-        
+
         for field, value in data.items():
+            # check the model attributes are matched and then save the values
             if hasattr(don, field):
                 setattr(don, field, value)
         if self.request.user.is_authenticated:
             don.user = self.request.user
         don.save()
         return JsonResponse({"success": True})
-        
 
 
-class PaymentErrorPageView(TemplateView):
+class PaymentErrorPageView(LoginRequiredMixin, TemplateView):
     template_name = 'payment_error.html'
-
-
